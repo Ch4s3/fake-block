34291af8c8ec4f699492b3bf0cec86501042b000
Chase <chase.gilliam@gmail.com>
2014-03-31 21:44:11 -0400

    fix usr mistake

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401000438) do
+ActiveRecord::Schema.define(version: 20140401014118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blocks", force: true do |t|
     t.text     "slab"
-    t.integer  "usr_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
