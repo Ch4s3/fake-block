a369870fd371502c1c6b486e6f9e6f5a932bac19
Chase <chase.gilliam@gmail.com>
2014-04-14 17:34:06 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,12 @@ puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
 user.add_role :admin
+
+[["kghosh1@students.towson.edu", "kajari"],
+ ["chase.gilliam@gmail.com", "chase"],
+ ["alex.ehrensberger@gmail.com", "alex"]
+].each do |email, name|
+  u = User.new(email: email, name: name, password: "12341234")
+  u.add_role :admin
+  u.save
+end
