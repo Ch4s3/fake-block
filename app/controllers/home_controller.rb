4a362ad4a2e0f9dc47b1106d73008c3aae94d068
Chase <chase.gilliam@gmail.com>
2014-05-19 08:59:04 -0400

    block fixes and refactored @user to current_user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,6 @@ class HomeController < ApplicationController
     @private_message = PrivateMessage.new
     @block = Block.new
     @blocks = Block.all.order(:cached_votes_up).reverse
-    if current_user.present?
-      @user ||= current_user
-    else
-      @user = nil
-    end
-   @comment =  Comment.new
+    @comment =  Comment.new
   end
 end
